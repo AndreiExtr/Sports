9b2fcb6f7372c8c237f22b701c4bdd58743f59ac
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-08-27 20:38:21 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Sports/'
-    : '/'
-}
+    : '/',
+  css: {
+    extract: {
+      filename: 'style.css',
+      chunkFilename: 'style.css'
+    }
+  }
+})
